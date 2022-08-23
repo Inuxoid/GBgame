1046ce421386fe59a40b4e2d9af66ef7ed09fd02
Inuxoid <alpha.inuxoid@gmail.com>
2022-08-23 05:03:42 +0300

    Revert "fixes"
    This reverts commit e5101c6d9aedb273cd59983b54da2f120c838d19.

--- Assets/Scripts/Player/CrouchHeadChecker.cs
+++ Assets/Scripts/Player/CrouchHeadChecker.cs
@@ -20,7 +20,7 @@ public class CrouchHeadChecker : MonoBehaviour
         if ((Physics.Raycast(crouchRayUp, out hit, standDistance) || 
             Physics.Raycast(crouchRayRightUp, out hit, standDistance) || 
             Physics.Raycast(crouchRayLeftUp, out hit, standDistance)) && 
-            hit.collider.CompareTag("Ceil"))
+            hit.collider.CompareTag("Ground"))
         {
             this.onCantStandUp?.Invoke();
         }
