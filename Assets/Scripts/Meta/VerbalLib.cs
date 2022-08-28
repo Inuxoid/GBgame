882befb1f0c96cae73874b0f7f67c7c1e4b345b4
PerspecTea <111482798+PerspecTea@users.noreply.github.com>
2022-08-28 14:44:48 +0300

    Fix lvl1

--- Assets/Scripts/Meta/VerbalLib.cs
+++ Assets/Scripts/Meta/VerbalLib.cs
@@ -24,7 +24,7 @@ class VerbalLib
     };
 
     public static char[] FirstCrouch = "Протискиваться в глубь вонючей ямы,\nИменумой \"городской канализацией\"...\nНе самая приятная затея".ToCharArray();
-    public static char[] FirstVHS = "Да не умер Гайан Рослинг в конце фильма...".ToCharArray();
+    public static char[] FirstVHS = "Классная кассета. И пусть катятся к черту те, кто сказал что Он умер в конце фильма".ToCharArray();
     public static char[] FirstPuddle = "Ну и запашок...\nПравда, вода в городе не сильно лучше.".ToCharArray();
     public static char[] FirstTurret = "Для защиты населения, говорили они.\nДа я прямо Ахиллес.\nПравда все тело – пята.".ToCharArray();
     public static char[] Elevator = "На удивление, я не испытал радости освобождения.\nЯ все еще был заперт в темнице.\nВ тюрьме иного толка...".ToCharArray();
