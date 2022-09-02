bc87fd9b4b8e4d02267429021bb27b6af2f3ff4f
Inuxoid <alpha.inuxoid@gmail.com>
2022-09-02 15:26:30 +0300

    punch with weird bug

--- Assets/Scripts/Player/Fight.cs
+++ Assets/Scripts/Player/Fight.cs
@@ -6,12 +6,14 @@ public class Fight : MonoBehaviour
 {
     [SerializeField] private int flip = 1;
     [SerializeField] private int damage;
+    [SerializeField] private Animator animator;
     public void Fliped()
     {
         flip = flip * -1;
     }
     public void Strike()
     {
+        animator.SetBool("isPunching", true);
         foreach (var item in Physics.OverlapBox(new Vector3(this.transform.position.x + flip, this.transform.position.y), 
                                                 new Vector3 (0.7f, 0.7f, 0.7f), 
                                                 Quaternion.identity, 128))
@@ -23,7 +25,7 @@ public class Fight : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Input.GetAxisRaw("Horizontal") == 0)
         {
             if (!gameObject.GetComponent<PlayerMovement>().Crouch)
             {
---
+++ Assets/Scripts/Player/UnPunch.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnPunch : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    public void EndPunch()
+    {
+        animator.SetBool("isPunching", false);
+        animator.SetBool("secondPunch", !animator.GetBool("secondPunch"));
+    }
+}
