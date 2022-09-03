75f3fbd83f2ef5d2784857fbb837a6af6a2b5841
Inuxoid <alpha.inuxoid@gmail.com>
2022-09-03 16:59:44 +0300

    Climb

--- Assets/Scripts/Player/ClimbData.cs
+++ Assets/Scripts/Player/ClimbData.cs
@@ -5,9 +5,21 @@ using UnityEngine.Events;
 
 public class ClimbData : MonoBehaviour
 {
-    [SerializeField] private Transform point1;
-    [SerializeField] private Transform point2;
+    [SerializeField] private Vector3 point1;
+    [SerializeField] private Vector3 point2;
+    [SerializeField] private float x1;
+    [SerializeField] private float y1;
+    [SerializeField] private float x2;
+    [SerializeField] private float y2;
+    [SerializeField] private float z;
+    [SerializeField] private float mult;
 
-    public Transform Point1 { get => point1; set => point1 = value; }
-    public Transform Point2 { get => point2; set => point2 = value; }
+    public Vector3 Point1 { get => point1; set => point1 = value; }
+    public Vector3 Point2 { get => point2; set => point2 = value; }
+
+    private void Start()
+    {
+        point1 = new Vector3(transform.position.x + x1 * mult, transform.position.y + y1, z);
+        point2 = new Vector3(transform.position.x + x2 * mult, transform.position.y + y2, z);
+    }
 }
