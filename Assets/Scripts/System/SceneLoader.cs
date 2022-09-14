346830a30b71fe7b4bc223a665c82c752139658a
Inuxoid <alpha.inuxoid@gmail.com>
2022-09-14 23:57:31 +0300

    CS

--- Assets/Scripts/System/SceneLoader.cs
+++ Assets/Scripts/System/SceneLoader.cs
@@ -23,7 +23,7 @@ public class SceneLoader : MonoBehaviour
         if (videoPlayer != null)
         {
             videoPlayer.Play();
-            yield return new WaitForSeconds(5f);
+            yield return new WaitForSeconds(209f);
         }
         SceneManager.LoadScene(sceneId);
 
