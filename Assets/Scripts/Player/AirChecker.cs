d3a3a38482741044eaff7df45abafccce5429db2
Inuxoid <alpha.inuxoid@gmail.com>
2022-09-28 13:17:40 +0300

    fixes

--- Assets/Scripts/Player/AirChecker.cs
+++ Assets/Scripts/Player/AirChecker.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
+using static UnityEditor.Progress;
 
 public class AirChecker : MonoBehaviour
 {
@@ -11,47 +14,75 @@ public class AirChecker : MonoBehaviour
 
     [Header("Settings")]
     [SerializeField] private float airDistance;
+    [SerializeField] private Vector3 bigCast = new Vector3(0.3f, 1.83f, 0.7f);
+    [SerializeField] private Vector3 smallCast = new Vector3(0.3f, 1.44f, 0.7f);
+    [SerializeField] private Vector3 nullCast = Vector3.zero;
+    [SerializeField] private Vector3 curCast;
+
+    private void OnDrawGizmosSelected()
+    {
+        if (playerMovement.Crouch)
+            curCast = nullCast;
+        else if (playerMovement.IsGrounded)
+            curCast = bigCast;
+        else
+            curCast = smallCast;
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(new Vector3(this.transform.position.x +
+                            (float)playerMovement.IntFlip / 5,
+                            this.transform.position.y - 0.18f,
+                            this.transform.position.z),
+                            curCast);
+    }
 
     private void Update()
     {
-        RaycastHit hit = new(); 
-        RaycastHit hit1 = new();
-        RaycastHit hit2 = new();
-        RaycastHit hit3 = new();
-        RaycastHit hit4 = new();
-        RaycastHit hit5 = new();
+        if (playerMovement.Crouch)
+            curCast = nullCast;
+        else if (playerMovement.IsGrounded)
+            curCast = bigCast;
+        else 
+            curCast = smallCast;
 
 
-        Ray airRayTopRight = new Ray(new Vector3(transform.position.x, transform.position.y + 0.7f), Vector3.right);
-        Ray airRayBotRight = new Ray(new Vector3(transform.position.x, transform.position.y - 0.5f), Vector3.right);
-        Ray airRayRight = new Ray(transform.position, Vector3.right);
+        List<Collider> colliders = Physics.OverlapBox(new Vector3(this.transform.position.x + 
+                                                                  (float)playerMovement.IntFlip / 3, 
+                                                                  this.transform.position.y - 0.18f, 
+                                                                  this.transform.position.z),
+                                                                  curCast).Where(x => (x.GetComponent<ClimbData>() != null || x.CompareTag("Wall")) && 
+                                                                  Math.Abs(x.transform.position.x - transform.position.x) > 0.6f).ToList();
 
-        Ray airRayTopLeft = new Ray(new Vector3(transform.position.x, transform.position.y + 0.7f), Vector3.left);
-        Ray airRayBotLeft = new Ray(new Vector3(transform.position.x, transform.position.y - 0.5f), Vector3.left);
-        Ray airRayLeft = new Ray(transform.position, Vector3.left);
-        if (Physics.Raycast(airRayRight, out hit, airDistance) ||
-            Physics.Raycast(airRayLeft, out hit1, airDistance) ||
-            Physics.Raycast(airRayTopRight, out hit2, airDistance) ||
-            Physics.Raycast(airRayBotRight, out hit3, airDistance) ||
-            Physics.Raycast(airRayTopLeft, out hit4, airDistance) ||
-            Physics.Raycast(airRayBotLeft, out hit5, airDistance))
+
+        //RaycastHit hit = new(); 
+        //RaycastHit hit1 = new();
+        //RaycastHit hit2 = new();
+        //RaycastHit hit3 = new();
+        //RaycastHit hit4 = new();
+        //RaycastHit hit5 = new();
+
+
+        //Ray airRayTopRight = new Ray(new Vector3(transform.position.x, transform.position.y + 0.7f), Vector3.right);
+        //Ray airRayBotRight = new Ray(new Vector3(transform.position.x, transform.position.y - 0.5f), Vector3.right);
+        //Ray airRayRight = new Ray(transform.position, Vector3.right);
+
+        //Ray airRayTopLeft = new Ray(new Vector3(transform.position.x, transform.position.y + 0.7f), Vector3.left);
+        //Ray airRayBotLeft = new Ray(new Vector3(transform.position.x, transform.position.y - 0.5f), Vector3.left);
+        //Ray airRayLeft = new Ray(transform.position, Vector3.left);
+
+        Debug.Log(colliders.Count);
+        if (colliders.Count != 0)
         {
-            List<RaycastHit> raycastHitList = new List<RaycastHit> { hit, hit1, hit2, hit3, hit4, hit5};
-
-            foreach (var item in raycastHitList)
+            foreach (var item in colliders)
             {
-                if (item.collider != null)
+                if (item.CompareTag("Ground"))
                 {
-                    if (item.collider.CompareTag("Ground"))
-                    {
-                        playerMovement.AirGroundCollision(item.collider.gameObject);
-                        break;
-                    }
-                    if (item.collider.CompareTag("Wall"))
-                    {
-                        playerMovement.AirWallCollision(item.collider.gameObject);
-                        break;
-                    }
+                    playerMovement.AirGroundCollision(item.gameObject);
+                    break;
+                }
+                if (item.CompareTag("Wall"))
+                {
+                    playerMovement.AirWallCollision(item.gameObject);
+                    break;
                 }
             }
         }
@@ -59,5 +90,37 @@ public class AirChecker : MonoBehaviour
         {
             playerMovement.AirWallUnCollision();
         }
+
+
+        //if (Physics.Raycast(airRayRight, out hit, airDistance) ||
+        //    Physics.Raycast(airRayLeft, out hit1, airDistance) ||
+        //    Physics.Raycast(airRayTopRight, out hit2, airDistance) ||
+        //    Physics.Raycast(airRayBotRight, out hit3, airDistance) ||
+        //    Physics.Raycast(airRayTopLeft, out hit4, airDistance) ||
+        //    Physics.Raycast(airRayBotLeft, out hit5, airDistance))
+        //{
+        //    List<RaycastHit> raycastHitList = new List<RaycastHit> { hit, hit1, hit2, hit3, hit4, hit5};
+
+        //    foreach (var item in raycastHitList)
+        //    {
+        //        if (item.collider != null)
+        //        {
+        //            if (item.collider.CompareTag("Ground"))
+        //            {
+        //                playerMovement.AirGroundCollision(item.collider.gameObject);
+        //                break;
+        //            }
+        //            if (item.collider.CompareTag("Wall"))
+        //            {
+        //                playerMovement.AirWallCollision(item.collider.gameObject);
+        //                break;
+        //            }
+        //        }
+        //    }
+        //}
+        //else
+        //{
+        //    playerMovement.AirWallUnCollision();
+        //}
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,140 +5,152 @@ using UnityEngine.Events;
 
 public class PlayerMovement : MonoBehaviour
 {
-  [Header("Components")] [SerializeField]
-  private Rigidbody rb;
+    [Header("Components")]
+    [SerializeField]
+    private Rigidbody rb;
 
-  [SerializeField] private Collider bodyCollider;
-  [SerializeField] private Collider visionCollider;
-  [SerializeField] private Animator animator;
-  [SerializeField] private Climb climb;
-  [SerializeField] private Fight fight;
-  [SerializeField] private Settings settings;
-  [SerializeField] private GameObject pausePanel;
-  [SerializeField] private GameObject body;
+    [SerializeField] private Collider bodyCollider;
+    [SerializeField] private Collider visionCollider;
+    [SerializeField] private Animator animator;
+    [SerializeField] private Climb climb;
+    [SerializeField] private Fight fight;
+    [SerializeField] private Settings settings;
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private GameObject body;
 
-  [Header("Settings")] [SerializeField] private bool facingRight = true;
+    [Header("Settings")][SerializeField] private bool facingRight = true;
 
-  [SerializeField] private float runSpeed;
-  [SerializeField] private float crouchSpeed;
-  [SerializeField] private float currentSpeed;
-  [SerializeField] private float jumpForce;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float crouchSpeed;
+    [SerializeField] private float currentSpeed;
+    [SerializeField] private float jumpForce;
 
-  [Range(0, .3f)] [SerializeField] private float movementSmoothing;
+    [Range(0, .3f)][SerializeField] private float movementSmoothing;
 
-  [SerializeField] private bool airControl = true;
-  [SerializeField] private float horizontalMove;
-  [SerializeField] private bool jump;
-  [SerializeField] private bool crouch;
-  [SerializeField] private bool isGrounded;
-  [SerializeField] private bool canDoubleJump;
-  [SerializeField] private float maxJumpTimer = 0.3f;
-  [SerializeField] private float currentJumpTimer;
+    [SerializeField] private bool airControl = true;
+    [SerializeField] private float horizontalMove;
+    [SerializeField] private bool jump;
+    [SerializeField] private bool crouch;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private bool canDoubleJump;
+    [SerializeField] private float maxJumpTimer = 0.3f;
+    [SerializeField] private float currentJumpTimer;
 
-  [Range(1, 2f)] [SerializeField] private bool canStandUp = true;
+    [Range(1, 2f)][SerializeField] private bool canStandUp = true;
 
-  [SerializeField] private bool WReleased;
-  [SerializeField] private bool SReleased;
-  [SerializeField] private UnityEvent flip;
-  [SerializeField] private float deltaSpeed;
-  [SerializeField] private float deltaSpeedMult;
-  [SerializeField] private Vector3 velocity = Vector3.zero;
-  [SerializeField] private float speedMod;
-  [SerializeField] private float airSpeed;
-  [SerializeField] private float intFlip = 1;
+    [SerializeField] private bool WReleased;
+    [SerializeField] private bool SReleased;
+    [SerializeField] private UnityEvent flip;
+    [SerializeField] private float deltaSpeed;
+    [SerializeField] private float deltaSpeedMult;
+    [SerializeField] private Vector3 velocity = Vector3.zero;
+    [SerializeField] private float speedMod;
+    [SerializeField] private float airSpeed;
+    [SerializeField] private float intFlip = 1;
     [SerializeField] private bool left;
 
+    [SerializeField] private Vector3 smallColl = new Vector3(0.6f, 0.6f, 0.6f);
+    [SerializeField] private Vector3 normColl = new Vector3(1f, 1f, 1f);
+    [SerializeField] private Vector3 deltCol = new Vector3(0f, 0.4f, 0f);
+    [SerializeField] private Vector3 nullCol = new Vector3(0f, -0.949f, 0f);
+
+
     public bool Crouch
-  {
-    get => crouch;
-    set => crouch = value;
-  }
-
-  public bool IsGrounded
-  {
-    get => isGrounded;
-    set => isGrounded = value;
-  }
-
-  public float SpeedMod
-  {
-    get => speedMod;
-    set => speedMod = value;
-  }
-
-  public bool AirControl
-  {
-    get => airControl;
-    set => airControl = value;
-  }
-
-  private void Update()
-  {
-    if (Input.GetKeyDown(KeyCode.Space) && Input.GetAxisRaw("Horizontal") == 0 && !animator.GetBool("isPunching"))
-      if (!Crouch)
-        fight.Strike();
-
-    horizontalMove = Input.GetAxisRaw("Horizontal") * SpeedMod;
-
-    animator.SetFloat("hSpeed", Math.Abs(Input.GetAxisRaw("Horizontal")));
-    animator.SetFloat("vSpeed", Math.Abs(GetComponent<Rigidbody>().velocity.y));
-
-    if (Math.Abs(Input.GetAxisRaw("Horizontal")) > 0)
     {
-      if (deltaSpeed < currentSpeed * 10) deltaSpeed += Time.deltaTime * deltaSpeedMult;
-    }
-    else
-    {
-      if (deltaSpeed > Time.deltaTime * deltaSpeedMult)
-        deltaSpeed -= Time.deltaTime * deltaSpeedMult;
-      else
-        deltaSpeed = 0;
+        get => crouch;
+        set => crouch = value;
     }
 
-    animator.SetFloat("hSpeed", deltaSpeed);
-
-    if (Input.GetKeyDown(KeyCode.W))
+    public bool IsGrounded
     {
-      jump = true;
-      WReleased = false;
+        get => isGrounded;
+        set => isGrounded = value;
     }
 
-    if (Input.GetKeyUp(KeyCode.W)) WReleased = true;
-
-    if (Input.GetKeyDown(KeyCode.S))
+    public float SpeedMod
     {
-      StartCrouch();
-      SReleased = false;
+        get => speedMod;
+        set => speedMod = value;
     }
 
-    if (Input.GetKeyUp(KeyCode.S)) SReleased = true;
-
-    if (Input.GetKeyDown(KeyCode.Escape))
+    public bool AirControl
     {
-      if (pausePanel.activeInHierarchy)
-      {
-        settings.Unpause();
-        pausePanel.SetActive(false);
-      }
-      else
-      {
-        settings.Pause();
-        pausePanel.SetActive(true);
-      }
+        get => airControl;
+        set => airControl = value;
+    }
+    public float IntFlip { get => intFlip; set => intFlip = value; }
+    public Vector3 SmallColl { get => smallColl; set => smallColl = value; }
+    public Vector3 NormColl { get => normColl; set => normColl = value; }
+    public Vector3 DeltCol { get => deltCol; set => deltCol = value; }
+    public Vector3 NullCol { get => nullCol; set => nullCol = value; }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Input.GetAxisRaw("Horizontal") == 0 && !animator.GetBool("isPunching"))
+            if (!Crouch)
+                fight.Strike();
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * SpeedMod;
+
+        animator.SetFloat("hSpeed", Math.Abs(Input.GetAxisRaw("Horizontal")));
+        animator.SetFloat("vSpeed", Math.Abs(GetComponent<Rigidbody>().velocity.y));
+
+        if (Math.Abs(Input.GetAxisRaw("Horizontal")) > 0)
+        {
+            if (deltaSpeed < currentSpeed * 10) deltaSpeed += Time.deltaTime * deltaSpeedMult;
+        }
+        else
+        {
+            if (deltaSpeed > Time.deltaTime * deltaSpeedMult)
+                deltaSpeed -= Time.deltaTime * deltaSpeedMult;
+            else
+                deltaSpeed = 0;
+        }
+
+        animator.SetFloat("hSpeed", deltaSpeed);
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            jump = true;
+            WReleased = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.W)) WReleased = true;
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            StartCrouch();
+            SReleased = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.S)) SReleased = true;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pausePanel.activeInHierarchy)
+            {
+                settings.Unpause();
+                pausePanel.SetActive(false);
+            }
+            else
+            {
+                settings.Pause();
+                pausePanel.SetActive(true);
+            }
+        }
+
+
+        if (Crouch && SReleased && canStandUp) EndCrouch();
     }
 
+    private void FixedUpdate()
+    {
+        Move(horizontalMove * Time.fixedDeltaTime, Crouch, jump);
+        jump = false;
+    }
 
-    if (Crouch && SReleased && canStandUp) EndCrouch();
-  }
-
-  private void FixedUpdate()
-  {
-    Move(horizontalMove * Time.fixedDeltaTime, Crouch, jump);
-    jump = false;
-  }
-
-  public void AirGroundCollision(GameObject go)
-  {
+    public void AirGroundCollision(GameObject go)
+    {
         left = true;
         if ((go.transform.position.x - transform.position.x) < 0)
         {
@@ -155,151 +167,155 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-  public void AirWallCollision(GameObject go)
-  {
-    if (Input.GetAxisRaw("Horizontal") * (go.transform.position.x - transform.position.x) > 0)
+    public void AirWallCollision(GameObject go)
     {
-      currentSpeed = 0;
-      SpeedMod = 0;
-    }
-    else
-    {
-      if (IsGrounded)
-        SpeedMod = 1;
-      else
-        SpeedMod = airSpeed;
+        if (Input.GetAxisRaw("Horizontal") * (go.transform.position.x - transform.position.x) > 0)
+        {
+            currentSpeed = 0;
+            SpeedMod = 0;
+        }
+        else
+        {
+            if (IsGrounded)
+                SpeedMod = 1;
+            else
+                SpeedMod = airSpeed;
 
-      if (Crouch)
+            if (Crouch)
+                currentSpeed = crouchSpeed;
+            else
+                currentSpeed = runSpeed;
+        }
+    }
+
+    public void AirWallUnCollision()
+    {
+        if (Crouch)
+        {
+            currentSpeed = crouchSpeed;
+            if (IsGrounded)
+                SpeedMod = 1;
+            else
+                SpeedMod = airSpeed;
+        }
+        else
+        {
+            currentSpeed = runSpeed;
+            if (IsGrounded)
+                SpeedMod = 1;
+            else
+                SpeedMod = airSpeed;
+        }
+    }
+
+    public void ToGround()
+    {
+        IsGrounded = true;
+        WReleased = false;
+        animator.SetBool("isJumping", false);
+        SpeedMod = 1;
+        bodyCollider.transform.localPosition = nullCol;
+        if (!Crouch)
+        {
+            bodyCollider.transform.localScale = normColl;
+        }
+        else
+            StartCrouch();
+    }
+
+    public void ToAir()
+    {
+        IsGrounded = false;
+        animator.SetBool("isJumping", true);
+        SpeedMod = airSpeed;
+        bodyCollider.transform.localPosition += deltCol;
+        bodyCollider.transform.localScale = smallColl;
+    }
+
+    public void CanStandUp()
+    {
+        canStandUp = true;
+    }
+
+    public void CantStandUp()
+    {
+        canStandUp = false;
+    }
+
+    public void Move(float move, bool crouch, bool jump)
+    {
+        if ((IsGrounded || AirControl) && climb.StartedClimbing == false)
+        {
+            // Vector3 targetVelocity = new Vector2(move * 10f, rb.velocity.y);
+            // rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, movementSmoothing);
+
+            rb.velocity = new Vector2(move * animator.GetFloat("hSpeed"), rb.velocity.y);
+
+            if (move > 0 && !facingRight)
+                Flip();
+
+            else if (move < 0 && facingRight) Flip();
+            if (move > 0) animator.SetBool("isPunching", false);
+        }
+
+        if (IsGrounded && jump && !crouch)
+        {
+            StartCoroutine(JumpTimer());
+            canDoubleJump = true;
+        }
+
+        //else if (canDoubleJump && jump && !crouch)
+        //{
+        //	canDoubleJump = false;
+        //	StartCoroutine(JumpTimer());
+        //}
+    }
+
+    public void StartCrouch()
+    {
+        Crouch = true;
         currentSpeed = crouchSpeed;
-      else
+        animator.SetBool("isCrouch", true);
+        deltaSpeed = 0;
+        bodyCollider.transform.localScale = smallColl;
+        visionCollider.transform.localScale = smallColl;
+        //body.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+    }
+
+    public void EndCrouch()
+    {
+        Crouch = false;
         currentSpeed = runSpeed;
+        animator.SetBool("isCrouch", false);
+        bodyCollider.transform.localScale = normColl;
+        visionCollider.transform.localScale = normColl;
+        //body.transform.localScale = new Vector3(1f, 1f, 1f);
     }
-  }
 
-  public void AirWallUnCollision()
-  {
-    if (Crouch)
+    private void Flip()
     {
-      currentSpeed = crouchSpeed;
-      if (IsGrounded)
-        SpeedMod = 1;
-      else
-        SpeedMod = airSpeed;
+        flip?.Invoke();
+        facingRight = !facingRight;
+        IntFlip *= -1;
+        //Vector3 theScale = transform.localScale;
+        //theScale.x *= -1;
+        //transform.localScale = theScale;
+
+        body.transform.Rotate(0, 180, 0);
     }
-    else
+
+
+    private IEnumerator JumpTimer()
     {
-      currentSpeed = runSpeed;
-      if (IsGrounded)
-        SpeedMod = 1;
-      else
-        SpeedMod = airSpeed;
+        currentJumpTimer = 0f;
+        while (currentJumpTimer < maxJumpTimer)
+        {
+            if (WReleased) break;
+            currentJumpTimer += 0.1f;
+            rb.AddForce(new Vector2(0f, jumpForce * (maxJumpTimer - currentJumpTimer)), ForceMode.Force);
+            yield return new WaitForSeconds(.1f);
+        }
+
+        yield return null;
     }
-  }
-
-  public void ToGround()
-  {
-    IsGrounded = true;
-    WReleased = false;
-    animator.SetBool("isJumping", false);
-    SpeedMod = 1;
-    if (!Crouch)
-      bodyCollider.transform.localScale = new Vector3(1f, 1f, 1f);
-    else
-      StartCrouch();
-  }
-
-  public void ToAir()
-  {
-    IsGrounded = false;
-    animator.SetBool("isJumping", true);
-    SpeedMod = airSpeed;
-    bodyCollider.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
-  }
-
-  public void CanStandUp()
-  {
-    canStandUp = true;
-  }
-
-  public void CantStandUp()
-  {
-    canStandUp = false;
-  }
-
-  public void Move(float move, bool crouch, bool jump)
-  {
-    if ((IsGrounded || AirControl) && climb.StartedClimbing == false)
-    {
-      // Vector3 targetVelocity = new Vector2(move * 10f, rb.velocity.y);
-      // rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, movementSmoothing);
-
-      rb.velocity = new Vector2(move * animator.GetFloat("hSpeed"), rb.velocity.y);
-
-      if (move > 0 && !facingRight)
-        Flip();
-
-      else if (move < 0 && facingRight) Flip();
-      if (move > 0) animator.SetBool("isPunching", false);
-    }
-
-    if (IsGrounded && jump && !crouch)
-    {
-      StartCoroutine(JumpTimer());
-      canDoubleJump = true;
-    }
-
-    //else if (canDoubleJump && jump && !crouch)
-    //{
-    //	canDoubleJump = false;
-    //	StartCoroutine(JumpTimer());
-    //}
-  }
-
-  public void StartCrouch()
-  {
-    Crouch = true;
-    currentSpeed = crouchSpeed;
-    animator.SetBool("isCrouch", true);
-    deltaSpeed = 0;
-    bodyCollider.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
-    visionCollider.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
-    //body.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
-  }
-
-  public void EndCrouch()
-  {
-    Crouch = false;
-    currentSpeed = runSpeed;
-    animator.SetBool("isCrouch", false);
-    bodyCollider.transform.localScale = new Vector3(1f, 1f, 1f);
-    visionCollider.transform.localScale = new Vector3(1f, 1f, 1f);
-    //body.transform.localScale = new Vector3(1f, 1f, 1f);
-  }
-
-  private void Flip()
-  {
-    flip?.Invoke();
-    facingRight = !facingRight;
-    intFlip *= -1;
-    //Vector3 theScale = transform.localScale;
-    //theScale.x *= -1;
-    //transform.localScale = theScale;
-
-    body.transform.Rotate(0, 180, 0);
-  }
-
-
-  private IEnumerator JumpTimer()
-  {
-    currentJumpTimer = 0f;
-    while (currentJumpTimer < maxJumpTimer)
-    {
-      if (WReleased) break;
-      currentJumpTimer += 0.1f;
-      rb.AddForce(new Vector2(0f, jumpForce * (maxJumpTimer - currentJumpTimer)), ForceMode.Force);
-      yield return new WaitForSeconds(.1f);
-    }
-
-    yield return null;
-  }
 }
