f53f6ea5ea9ebd668091e886f2d3d75b230770f1
Inuxoid <alpha.inuxoid@gmail.com>
2022-10-21 22:21:51 +0300

    Update AudioLib.cs

--- Assets/Scripts/System/AudioLib.cs
+++ Assets/Scripts/System/AudioLib.cs
@@ -4,18 +4,38 @@ using UnityEngine;
 
 public class AudioLib : MonoBehaviour
 {
-    public void PlaySound(AudioClip clip, AudioSource audioSource)
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioSource audioSourceRun;
+    [SerializeField] private AudioSource audioSourceClimb;
+    [SerializeField] private AudioSource audioSourceCrouch;
+
+    public void PlaySound(AudioClip clip)
     {
         audioSource.clip = clip;
         audioSource.Play();
     }
 
-    public void PlayOnce(AudioClip clip, AudioSource audioSource)
+    public void PlayOnce(AudioClip clip)
     {
         audioSource?.PlayOneShot(clip);
     }
 
-    public void StopPlaying(AudioSource audioSource)
+    public void PlayOnceRun(AudioClip clip)
+    {
+        audioSourceRun?.PlayOneShot(clip);
+    }
+
+    public void PlayOnceClimb(AudioClip clip)
+    {
+        audioSourceClimb?.PlayOneShot(clip);
+    }
+
+    public void PlayOnceCrouch(AudioClip clip)
+    {
+        audioSourceCrouch?.PlayOneShot(clip);
+    }
+
+    public void StopPlaying()
     {
         audioSource.Stop();
     }
