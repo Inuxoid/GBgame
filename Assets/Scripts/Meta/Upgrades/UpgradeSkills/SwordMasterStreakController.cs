ca97676e76379fb498591de008df03bded0b7dde
Inuxoid <alpha.inuxoid@gmail.com>
2023-08-27 19:56:42 +0300

    Streak part2

---
+++ Assets/Scripts/Meta/Upgrades/UpgradeSkills/SwordMasterStreakController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Meta.Upgrades.UpgradeSkills
+{
+    public class SwordMasterStreakController : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+
+        public void UnStreak()
+        {
+            animator.SetInteger("punchStreak", 0);
+            animator.SetBool("secondPunch", false);
+        }
+    }
+}
