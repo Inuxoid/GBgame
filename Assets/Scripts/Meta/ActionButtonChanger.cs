571213c3622560392aea75520fb1dfd3baa106ea
Inuxoid <alpha.inuxoid@gmail.com>
2023-10-01 21:28:13 +0300

    ActionButtonChanger
    Also add observer in state machine

---
+++ Assets/Scripts/Meta/ActionButtonChanger.cs
@@ -0,0 +1,32 @@
+﻿using StateMachines.PlayerSM;
+using UI;
+using UnityEngine;
+
+namespace Meta
+{
+    public class ActionButtonChanger : MonoBehaviour
+    {
+        [SerializeField] private ActionButton buttonIconUpdater;
+
+        private void Awake()
+        {
+            buttonIconUpdater = FindObjectOfType<ActionButton>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.GetComponentInParent<PlayerSM>())
+            {
+                buttonIconUpdater.OnActionNear(true);
+            }
+        }
+        
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.GetComponentInParent<PlayerSM>())
+            {
+                buttonIconUpdater.OnActionNear(false);
+            }
+        }
+    }
+}
