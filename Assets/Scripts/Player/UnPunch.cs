6a738f1f4d1bd71411021c95d1a7e723051cf2a5
Inuxoid <alpha.inuxoid@gmail.com>
2023-10-02 13:14:22 +0300

    unPunch swordMaster refactor

--- Assets/Scripts/Player/UnPunch.cs
+++ Assets/Scripts/Player/UnPunch.cs
@@ -3,58 +3,111 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UnPunch : MonoBehaviour
-{ 
+{
     [SerializeField] private Animator animator;
+    [SerializeField] private float resetTime = 3.0f;
+    private Coroutine punchTimerCoroutine;
 
+    private void Update()
+    {
+        if (animator.GetBool("isPunching"))
+        {
+            if (punchTimerCoroutine != null)
+            {
+                StopCoroutine(punchTimerCoroutine);
+                punchTimerCoroutine = null;
+            }
+        }
+        else
+        {
+            if (punchTimerCoroutine == null)
+            {
+                punchTimerCoroutine = StartCoroutine(PunchTimer());
+            }
+        }
+    }
+
+    private IEnumerator PunchTimer()
+    {
+        yield return new WaitForSeconds(resetTime);
+        ResetPunch();
+    }
+    
     public void EndPunch()
+    {
+        ResetAnimatorBools();
+        
+        int punchStreakValue = animator.GetInteger("punchStreak");
+        
+        if (punchStreakValue == 3 || animator.GetBool("isPunchingWeak"))
+        {
+            ResetPunch();
+        }
+        else if (punchStreakValue == 2)
+        {
+            HandleSecondPunchCombo();
+        }
+        else if (punchStreakValue == 1)
+        {
+            HandleFirstPunchCombo();
+        }
+        else
+        {
+            HandleInitialPunch();
+        }
+    }
+
+    private void ResetAnimatorBools()
     {
         animator.SetBool("isPunching", false);
         animator.SetBool("isStrongPunching", false);
         animator.SetBool("isStrongPunchingFinish", false);
         animator.SetBool("isPunchingWeak", false);
         animator.SetBool("isShooting", false);
-        
-        if (animator.GetInteger("punchStreak") == 3)
-        {
-            animator.SetInteger("punchStreak", 0);
-            animator.SetBool("secondPunch", false);
-            return;
-        }
-        
-        if (animator.GetBool("FinishCombo") && animator.GetInteger("punchStreak") == 2)
-        {
-            animator.SetInteger("punchStreak", 3);    
-            return;
-        }
-        else if (!animator.GetBool("FinishCombo"))
-        {
-            animator.SetInteger("punchStreak", 0);
-            animator.SetBool("secondPunch", false);
-            return;
-        }
+    }
+    
+    private void ResetPunch()
+    {
+        animator.SetInteger("punchStreak", 0);
+        animator.SetBool("secondPunch", false);
+    }
 
-        if (animator.GetBool("SecondCombo") && animator.GetInteger("punchStreak") == 1)
+    private void HandleSecondPunchCombo()
+    {
+        if (animator.GetBool("FinishCombo"))
         {
-            animator.SetInteger("punchStreak", 2);
-            return;
-        }
-        else if (!animator.GetBool("SecondCombo"))
-        {
-            animator.SetInteger("punchStreak", 0);
-            animator.SetBool("secondPunch", false);
-            return;
-        }
-                                            
-        if (animator.GetBool("secondPunch") && animator.GetBool("FirstCombo") && animator.GetInteger("punchStreak") == 0)
-        {
-            animator.SetInteger("punchStreak", 1);
-            animator.SetBool("secondPunch", false);
-            return;
+            animator.SetInteger("punchStreak", 3);
         }
         else
         {
-            animator.SetBool("secondPunch", true);
-            return;
-        }    
+            animator.SetInteger("punchStreak", 0);
+            animator.SetBool("secondPunch", false);
+        }
+    }
+
+    private void HandleFirstPunchCombo()
+    {
+        if (animator.GetBool("SecondCombo"))
+        {
+            animator.SetInteger("punchStreak", 2);
+        }
+        else
+        {
+            animator.SetInteger("punchStreak", 0);
+            animator.SetBool("secondPunch", false);
+        }
+    }
+
+    private void HandleInitialPunch()
+    {
+        if (animator.GetBool("secondPunch") && animator.GetBool("FirstCombo"))
+        {
+            animator.SetInteger("punchStreak", 1);
+            animator.SetBool("secondPunch", false);
+        }
+        else
+        {
+            animator.SetBool("secondPunch", !animator.GetBool("secondPunch"));
+        }
     }
 }
