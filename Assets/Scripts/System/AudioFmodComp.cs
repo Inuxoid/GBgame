b6359456dc5687636ca392e76aa37aa2dcfa7c57
Aestazis <intrelogic@mail.ru>
2023-11-30 14:40:37 +0300

    1-1 test computer sound

---
+++ Assets/Scripts/System/AudioFmodComp.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioFmodComp : MonoBehaviour
+{
+    private ParticleSystem _parentParticleSystem;
+
+private int _currentNumberOfParticles = 0;
+
+private void Start()
+
+{
+
+_parentParticleSystem = this.GetComponent<ParticleSystem>();
+
+}
+
+void Update()
+
+{
+
+if (_parentParticleSystem.particleCount < _currentNumberOfParticles)
+
+{
+
+}
+
+if (_parentParticleSystem.particleCount > _currentNumberOfParticles)
+
+{
+
+FMODUnity.RuntimeManager.PlayOneShotAttached("event:/death1-1", gameObject);
+
+}
+
+_currentNumberOfParticles = _parentParticleSystem.particleCount;
+
+}
+}
